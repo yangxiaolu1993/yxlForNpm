73aab4e07f06fe7e4730c9b0a7ef2b7950629c3c
yangxiaolu3 <yangxiaolu3@jd.com>
2021-02-26 18:32:26 +0800

    按需加载

--- src/components/index.js
+++ src/components/index.js
@@ -1,36 +1,26 @@
-import Vue from "vue";
+
 import textA from "./textA/index.vue";
 import textB from "./textB/index.vue";
 
 
-
-
-const install = function(Vue,opts = {}) {
-
-}
 const Components = {
     textA,
     textB
 };
 
-Object.keys(Components).forEach(name => {
-    Vue.component(name, Components[name]);
-});
+
+const install = function(Vue) {
+    Object.keys(Components).forEach(name => {
+        Vue.component(name, Components[name]);
+    });
+}
 
 if (typeof window !== 'undefined' && window.Vue) {
     install(window.Vue);
 }
 
 export default {
-    install
+    install,
+    ...Components
 };
 
-
-// import testA from './textA/index.js'
-// import testB from './textB/index.js'
-// export default {
-//     install(Vue) {
-//         Vue.use(testA);
-//         Vue.use(testB)
-//     },
-// }
